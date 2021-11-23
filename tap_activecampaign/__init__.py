a7c5f2018228b71ffa22d7e8bbe65455647e9351
brent-yoast <84784167+brent-yoast@users.noreply.github.com>
2021-11-23 14:44:42 +0100

    Update __init__.py

--- tap_activecampaign/__init__.py
+++ tap_activecampaign/__init__.py
@@ -22,8 +22,10 @@ def do_discover():
     catalog = discover()
     LOGGER.info('~~~~~Made it out of discover function call~~~~~')
     LOGGER.info('catalog data: {}'.format(catalog))
-    json.dump(catalog.to_dict(), sys.stdout, indent=2)
+    # json.dump(catalog.to_dict(), sys.stdout, indent=2)
+    catalog_string = json.dumps(catalog.to_dict(), indent=2)
     LOGGER.info('Finished discover')
+    return catalog_string
 
 
 @singer.utils.handle_top_exception(LOGGER)
